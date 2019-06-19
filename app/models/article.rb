5e927f1ad4f67cf8d926a3a31783f45196e53f59
yusukefield <kma14second@icloud.com>
2019-06-19 05:06:28 +0000

    [finish]article-tag,map

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
     belongs_to :user
     has_many :article_pictures, dependent: :destroy
     accepts_attachments_for :article_pictures, attachment: :picture
+    geocoded_by :address
+    after_validation :geocode
     acts_as_taggable
     enum category:{report: 0,item: 1,coodinate: 2}
 end
